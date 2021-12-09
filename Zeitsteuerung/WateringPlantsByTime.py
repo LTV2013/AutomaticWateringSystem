e7ba731ca40f7803c89a0d016691f8532deb07dc
LTV2013 <lasse@vierow.de>
2021-12-09 11:06:59 +0100

    script von montag

--- Zeitsteuerung/WateringPlantsByTime.py
+++ Zeitsteuerung/WateringPlantsByTime.py
@@ -1,19 +1,39 @@
 import RPi.GPIO as GPIO
 import time
+import board
+import busio
+i2c = busio.I2C(board.SCL, board.SDA)
+import schedule
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+ads = ADS.ADS1115(i2c)
+chan = AnalogIn(ads, ADS.P0)
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.OUT)
-GPIO.setup(3, GPIO.OUT)
 
 
-GPIO.output (4, GPIO.LOW)
-GPIO.output (3, GPIO.HIGH)
-print("test")
-time.sleep(3)
-print("test")
-GPIO.output (4, GPIO.HIGH)
-GPIO.output (3, GPIO.LOW)
-time.sleep(3)
+pumps=[5, 6, 13, 19]
 
+def setup():
+    for i in pumps:
+        GPIO.setup(i, GPIO.OUT)
 
+def water():
+    print(chan.value)
+    if chan.value < 20000:
+        for i in pumps:
+            GPIO.output (i, GPIO.LOW)
+            time.sleep(5)
+            GPIO.output (i, GPIO.HIGH)
+            print("Watered")
+
+def main():
+    setup()
+    water()
+    
+
+schedule.every(10).seconds.do(water)
+main()
+while True:
+    schedule.run_pending()
 GPIO.cleanup()
